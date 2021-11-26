9b5d0a774282468e59e7b5a585dbf470bec57087
Julian Fritzsch <me@julianfritzsch.de>
2021-11-26 11:49:58 +0100

    PSCC paper

--- readme_to_site.py
+++ readme_to_site.py
@@ -18,7 +18,7 @@ def read_comment(comments, entry, dictionnay, file):
     try:
         f_readme = open(file)
         for line in f_readme:
-            for comm,key in zip(comments,entry):
+            for comm, key in zip(comments, entry):
                 if line.lower().startswith(comm) and len(line[len(comm)+1:-4]) > 3:
                     dictionnay[key] = line[len(comm)+1:-4]
 
@@ -28,12 +28,12 @@ def read_comment(comments, entry, dictionnay, file):
         print(file)
 
 
-#Adding colours to make things more visually pleasing and understandable
-reset='\033[0m'
-title='\033[1;32m\033[4;32m'
-text='\033[0;32m'
-warning='\033[0;33m'
-error='\033[0;31m'
+# Adding colours to make things more visually pleasing and understandable
+reset = '\033[0m'
+title = '\033[1;32m\033[4;32m'
+text = '\033[0;32m'
+warning = '\033[0;33m'
+error = '\033[0;31m'
 
 file = open('README.md')
 
@@ -47,7 +47,7 @@ state = 0
 publication = []
 news = []
 presentations = []
-year=0
+year = 0
 for line in file:
 
     if line.find("## Publications") != -1:
@@ -58,67 +58,66 @@ for line in file:
         state = 3
 
     if line.find("###") != -1:
-        for y in [2020,2019,2018,2017,2016,2015,2014,2013,2012]:
+        for y in [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012]:
             if line.find(str(y)) != -1:
                 year = y
 
-
     if line.startswith('*'):
         if state == 1:
-            publication.append([line,str(year)])
+            publication.append([line, str(year)])
         elif state == 2:
-            news.append([line,str(year)])
+            news.append([line, str(year)])
         elif state == 3:
-            presentations.append([line,str(year)])
+            presentations.append([line, str(year)])
 
 
-publication_list=[]
+publication_list = []
 
-all_publication_dict={}
+all_publication_dict = {}
 
-all_publication_dict['publication']={}
+all_publication_dict['publication'] = {}
 
-count=0
-index=0
+count = 0
+index = 0
 for p_all in publication:
     # print(p)
-    p=p_all[0]
+    p = p_all[0]
     part_1 = p[p.find('[')+1:p.find(']')]
     part_2 = p[p.find(']')+2:-2]
     # print(part_1)
     # print(part_2)
 
-    publication_dict={}
-    publication_dict['authors']=part_1[:part_1.find('*')].rstrip(', ')
-    publication_dict['title']=part_1[part_1.find('*')+1:part_1.find('*',part_1.find('*')+1)].rstrip(', ')
+    publication_dict = {}
+    publication_dict['authors'] = part_1[:part_1.find('*')].rstrip(', ')
+    publication_dict['title'] = part_1[part_1.find(
+        '*')+1:part_1.find('*', part_1.find('*')+1)].rstrip(', ')
 
     try:
-        publication_dict['publisher']=part_1[part_1.find('*',part_1.find('*')+1)+3:part_1.find('(')].rstrip(', ')
+        publication_dict['publisher'] = part_1[part_1.find(
+            '*', part_1.find('*')+1)+3:part_1.find('(')].rstrip(', ')
     except:
-        publication_dict['publisher']=''
+        publication_dict['publisher'] = ''
 
     try:
-        publication_dict['date']=p_all[1]
+        publication_dict['date'] = p_all[1]
     except:
-        publication_dict['date']=''
+        publication_dict['date'] = ''
 
-
-    publication_dict['display']=part_1
+    publication_dict['display'] = part_1
 
     display_temp = publication_dict['display']
 
     for _ in range(3):
-        display_temp = display_temp.replace('**','<b>',1)
-        display_temp = display_temp.replace('**','</b>',1)
-    
+        display_temp = display_temp.replace('**', '<b>', 1)
+        display_temp = display_temp.replace('**', '</b>', 1)
+
     for _ in range(3):
-        display_temp = display_temp.replace('*','<i>',1)
-        display_temp = display_temp.replace('*','</i>',1)
-    
+        display_temp = display_temp.replace('*', '<i>', 1)
+        display_temp = display_temp.replace('*', '</i>', 1)
 
     publication_dict['display'] = display_temp
 
-    part_2  = part_2.replace('tree','blob',1)
+    part_2 = part_2.replace('tree', 'blob', 1)
     # print(part_2[part_2.find('master')+6:])
 
     name_pdf = ''
@@ -133,57 +132,56 @@ for p_all in publication:
                 elif name.endswith('.md'):
                     name_readme = name
 
-    
-    part_2_root = part_2 +'/'
+    part_2_root = part_2 + '/'
 
     if len(name_pdf) > 3:
-        publication_dict['url']=part_2_root+name_pdf
+        publication_dict['url'] = part_2_root+name_pdf
     else:
-        publication_dict['url']=part_2
+        publication_dict['url'] = part_2
 
     publication_dict['tags'] = ''
-
 
     if len(name_readme) > 3:
         # print(part_2_root + name_readme)
         try:
-            file_name=part_2[part_2.find('master')+7:] + '/' + name_readme
+            file_name = part_2[part_2.find('master')+7:] + '/' + name_readme
 
-            read_comment(['<!-- keywords:'],['tags'],publication_dict,file_name)
-            
+            read_comment(['<!-- keywords:'], ['tags'],
+                         publication_dict, file_name)
+
         except:
-            print(error+"Unable to open "+ part_2_root+name_readme+reset)
-            
+            print(error+"Unable to open " + part_2_root+name_readme+reset)
+
     else:
-        print(warning+"WARNING! No ReadMe for "+ part_2_root + reset)
+        print(warning+"WARNING! No ReadMe for " + part_2_root + reset)
 
-    name= 'p_' + (str(count).zfill(2))
+    name = 'p_' + (str(count).zfill(2))
     count += 1
-    all_publication_dict['publication'][name]=publication_dict
+    all_publication_dict['publication'][name] = publication_dict
 
-    
 
 with open('pub.json', 'w') as fp:
-    json.dump(all_publication_dict, fp,indent=4,sort_keys=True)
+    json.dump(all_publication_dict, fp, indent=4, sort_keys=True)
 
 ##############
 
 all_news_dict = {}
-all_news_dict['news']={}
+all_news_dict['news'] = {}
 
 count = 0
 
 
 for n_all in news:
-    n=n_all[0]
+    n = n_all[0]
     part_1 = n[n.find('[')+1:n.find(']')]
     part_2 = n[n.find(']')+2:n.find(')')]
 
-    news_dict={}
-    news_dict['authors']=''
-    news_dict['title']=part_1[:part_1.find('*')].rstrip(', ')
+    news_dict = {}
+    news_dict['authors'] = ''
+    news_dict['title'] = part_1[:part_1.find('*')].rstrip(', ')
 
-    news_dict['publisher'] = part_1[part_1.find('*')+1:part_1.find('*',part_1.find('*')+1)].rstrip(', ')
+    news_dict['publisher'] = part_1[part_1.find(
+        '*')+1:part_1.find('*', part_1.find('*')+1)].rstrip(', ')
     news_dict['date'] = n_all[1]
     news_dict['tags'] = ''
     news_dict['display'] = part_1
@@ -191,11 +189,10 @@ for n_all in news:
 
     display_temp = news_dict['display']
 
-    display_temp = display_temp.replace('*','<b>',1)
-    display_temp = display_temp.replace('*','</b>',1)
+    display_temp = display_temp.replace('*', '<b>', 1)
+    display_temp = display_temp.replace('*', '</b>', 1)
 
     news_dict['display'] = display_temp
-
 
     name_readme = ''
 
@@ -208,31 +205,30 @@ for n_all in news:
                 elif name.endswith('.md'):
                     name_readme = name
 
-    
-    part_2_root = part_2 +'/'
+    part_2_root = part_2 + '/'
 
     news_dict['tags'] = ''
 
     if len(name_readme) > 3:
-        file_name=part_2[part_2.find('master')+7:] + '/' + name_readme
+        file_name = part_2[part_2.find('master')+7:] + '/' + name_readme
 
-        read_comment(['<!-- keywords:','<!-- link:'],['tags','url'],news_dict,file_name)
+        read_comment(['<!-- keywords:', '<!-- link:'],
+                     ['tags', 'url'], news_dict, file_name)
     else:
         print(warning+"WARNING! No ReadMe for " + part_2_root + reset)
 
-
-    name='n_'+(str(count).zfill(2))
+    name = 'n_'+(str(count).zfill(2))
     count += 1
-    all_news_dict['news'][name]=news_dict
+    all_news_dict['news'][name] = news_dict
 
 with open('news.json', 'w') as fp:
-    json.dump(all_news_dict,fp,indent=4,sort_keys=True)
+    json.dump(all_news_dict, fp, indent=4, sort_keys=True)
 
 
 #################
 
 all_presentations_dict = {}
-all_presentations_dict['presentations']={}
+all_presentations_dict['presentations'] = {}
 
 count = 0
 
@@ -242,12 +238,14 @@ for pr_all in presentations:
     part_1 = pr[pr.find('[')+1:pr.find(']')]
     part_2 = pr[pr.find(']')+2:pr.find(')')]
 
-    pres_dict={}
-    pres_dict['authors']=''
+    pres_dict = {}
+    pres_dict['authors'] = ''
     #pres_dict['title']=part_1[:part_1.find('*')].rstrip(', ')
-    pres_dict['title']=part_1.replace('*','') #Remove the * for cleaner display
+    # Remove the * for cleaner display
+    pres_dict['title'] = part_1.replace('*', '')
 
-    pres_dict['publisher'] = part_1[part_1.find('*')+1:part_1.find('*',part_1.find('*')+1)].rstrip(', ')
+    pres_dict['publisher'] = part_1[part_1.find(
+        '*')+1:part_1.find('*', part_1.find('*')+1)].rstrip(', ')
     pres_dict['date'] = pr_all[1]
     pres_dict['tags'] = ''
     pres_dict['display'] = part_1
@@ -255,11 +253,10 @@ for pr_all in presentations:
 
     display_temp = pres_dict['display']
 
-    display_temp = display_temp.replace('*','<b>',1)
-    display_temp = display_temp.replace('*','</b>',1)
+    display_temp = display_temp.replace('*', '<b>', 1)
+    display_temp = display_temp.replace('*', '</b>', 1)
 
     pres_dict['display'] = display_temp
-
 
     name_readme = ''
 
@@ -272,27 +269,26 @@ for pr_all in presentations:
                 elif name.endswith('.md'):
                     name_readme = name
 
-    
-    part_2_root = part_2 +'/'
+    part_2_root = part_2 + '/'
 
     pres_dict['tags'] = ''
 
     if len(name_readme) > 3:
         # print(part_2_root + name_readme)
         try:
-            file_name=part_2[part_2.find('master')+7:] + '/' + name_readme
+            file_name = part_2[part_2.find('master')+7:] + '/' + name_readme
 
-            read_comment(['<!-- keywords:','<!-- link:'],['tags','url'],pres_dict,file_name)
+            read_comment(['<!-- keywords:', '<!-- link:'],
+                         ['tags', 'url'], pres_dict, file_name)
         except:
             print(error+"Unable to open " + part_2_root+name_readme+reset)
-            
+
     else:
-        print(warning+"WARNING! No ReadMe for "+ part_2_root+reset)
+        print(warning+"WARNING! No ReadMe for " + part_2_root+reset)
 
-
-    name='p_'+(str(count).zfill(2))
+    name = 'p_'+(str(count).zfill(2))
     count += 1
-    all_presentations_dict['presentations'][name]=pres_dict
+    all_presentations_dict['presentations'][name] = pres_dict
 
 with open('pres.json', 'w') as fp:
-    json.dump(all_presentations_dict,fp,indent=4,sort_keys=True)
+    json.dump(all_presentations_dict, fp, indent=4, sort_keys=True)
